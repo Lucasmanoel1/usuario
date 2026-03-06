198797cdfd539d75547af1c937286177b16ab2bb
Lucas <lucasmanoel.email@gmail.com>
2026-03-06 13:17:56 -0300

    adicionadas classes de security

---
+++ src/main/java/com/lucasmanoel/usuario/infrastructure/repository/EnderecoRepository.java
@@ -0,0 +1,10 @@
+package com.lucasmanoel.usuario.infrastructure.repository;
+
+
+import com.lucasmanoel.usuario.infrastructure.entity.Endereco;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EnderecoRepository extends JpaRepository<Endereco, Long> {
+}
